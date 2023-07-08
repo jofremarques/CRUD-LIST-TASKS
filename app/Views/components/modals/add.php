116ec8870f3529e3bfa355ee9fb65b0085007bb2
jofremarques <jofremartinsmarques@gmail.com>
2023-07-07 22:40:29 -0300

    resolved conflits

---
+++ app/Views/components/modals/add.php
@@ -0,0 +1,58 @@
+<div id="add-modal" class="modal">
+    <div class="content-modal">
+        <div class="head-modal flex">
+            <h1 class="title-modal">Adicionar nova tarefa</h1>
+            <span class="head-modal-fechar close-modal" onclick="closeModal('add-modal')">X</span>
+        </div>
+        <div class="body-modal">
+            <div class="row-modal flex justify-arround">
+                <div class="box">
+                    <div class="form-group">
+                        <label for="titulo">Titulo:</label>
+                        <input type="text" id="titulo">
+                    </div>
+                </div>
+                <div class="box ">
+                    <div class="form-group">
+                        <label for="descricao">Descrição:</label>
+                        <input type="text" id="descricao">
+                    </div>
+                </div>
+            </div>
+            <div class="row-modal flex justify-arround">
+                <div class="box">
+                    <div class="form-group">
+                        <label for="">Prioridade:</label>
+                        <select name="prioridade" id="prioridade">
+                            <option value="baixa">baixa</option>
+                            <option value="media">Media</option>
+                            <option value="alta">Alta</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="box ">
+                    <div class="form-group">
+                        <label for="">Status:</label>
+                        <select name="prioridade" id="prioridade">
+                            <option value="baixa">baixa</option>
+                            <option value="media">Media</option>
+                            <option value="alta">Alta</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+            <div class="row-modal">
+                <div class="box">
+                    <div class="form-group">
+                        <label for="">Prazo de entrega:</label>
+                        <input type="date" name="entrega" id="entrega">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footer-modal">
+            <button class="confirm-modal">Confirmar</button>
+            <button class="close-modal" onclick="closeModal('add-modal')">Fechar</button>
+        </div>
+    </div>
+</div>
---
+++ app/Views/globals/header.php
@@ -0,0 +1,5 @@
+<header>
+    <div class="logo-title">
+        <h1>Company tasks</h1>
+    </div>
+</header>
